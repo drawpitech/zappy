a1a39140dee0cc43e7b2a61cb4c170e13cdbb2d9
celian <celian.lombardot@epitech.eu>
2024-05-30 20:44:19 +0200

    feat: add multiprocess

--- AI/trentorian/trentorian.py
+++ AI/trentorian/trentorian.py
@@ -2,12 +2,17 @@
 """
 Module providing the trantorian class
 """
+from time import sleep
 from client.client import Client
+
+FOOD = 126
 
 class Trantorian:
     """Class for a trantorian
     """
     def __init__(self, host: str, port: int, team: str, pop : list):
+        self.host = host
+        self.port = port
         try:
             self.client = Client(host, port, team)
         except RuntimeError as err:
@@ -16,11 +21,29 @@ class Trantorian:
         self.pop: list = pop
         self.pop.append(self)
         self.team: str = team
-        self.life: int = 10
+        self.life: int = FOOD * 10
         self.inventory :dict = {
             "food": 0, "linemate": 0, "deraumere": 0,
             "sibur": 0, "mendiane": 0, "phiras": 0, "thystame": 0
         }
+
+    def born(self):
+        if self.forward():
+            print("Just moved forward")
+        else:
+            print("didn't move")
+        # Trantorian(self.)
+
+    def wait_answer(self) -> str:
+        """wait for an answer, handle the message and eject
+
+        Returns:
+            str: last server answer
+        """
+        answer: str = self.client.get_answer()
+        while answer[:7] == 'message' or answer[:5] == 'eject':
+            print(answer[:7])
+        return answer
 
     def forward(self) -> bool:
         """try to go forward
@@ -29,7 +52,9 @@ class Trantorian:
             bool: true if it worked, otherwise false
         """
         self.client.send_cmd("Forward")
-        answer = self.client.get_answer()
+        for i in range(100):
+            print(i)
+        answer = self.wait_answer()
         if answer == 'ok\n':
             return True
         return False

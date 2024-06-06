c1705b51afcb06672bf9fd70a855b985f853a716
celian <celian.lombardot@epitech.eu>
2024-06-06 12:39:51 +0200

    feat: function to check if incantation is possible

--- AI/utils.py
+++ AI/utils.py
@@ -24,7 +24,7 @@ def shortest_path_diff(a: int, b: int, size: int) -> int:
         a (int): first value
         b (int): second value
         size (int): size of the grid
-    
+
     Returns:
         int: shortest path difference
     """
@@ -68,3 +68,37 @@ def determine_direction(p1, p2, grid_size) -> SoundDirection:
             direction = SoundDirection.WEST
 
     return direction
+
+def check_levelup(inventory: dict, level: int, current_players: int) -> bool:
+    """check if the level up is possible given the current ressources
+
+    Args:
+        inventory (dict): current ressources
+        level (int): objective level
+        current_players (int): number of player on the same level
+
+    Returns:
+        bool: true if incantation is possible
+    """
+    elements = ["food", "linemate", "deraumere", "sibur", "mendiane", "phiras", "thystame"]
+    for elt in elements:
+        if elt not in inventory:
+            return False
+    levels = { # contain for each level, nbr player needed and ressource needed
+        2: (1,[1, 0, 0, 0, 0, 0]),
+        3: (1,[1, 1, 1, 0, 0, 0]),
+        4: (1,[2, 0, 1, 0, 2, 0]),
+        5: (1,[1, 1, 2, 0, 1, 0]),
+        6: (1,[1, 2, 1, 3, 0, 0]),
+        7: (1,[1, 2, 3, 0, 1, 0]),
+        8: (1,[2, 2, 2, 2, 2, 1]),
+    }
+    nbplayers, materials = levels[level]
+    if nbplayers > current_players:
+        return False
+    if inventory["food"] < 3 * nbplayers:
+        return False
+    for i in range(len(elements) - 1):
+        if inventory[elements[i + 1]] < materials[i]:
+            return False
+    return True

87ff1b550ee93f3324796824b77df4e8f625d893
Eliot <eliot1.martin@epitech.eu>
2024-06-18 00:02:37 +0200

    fix: revert message beacon changes

--- AI/parser/concrete/message_beacon_parser.py
+++ AI/parser/concrete/message_beacon_parser.py
@@ -5,13 +5,11 @@ from parser.concrete.message_type_parser import MessageType
 
 from warnings import warn
 from enum import IntEnum
-from time import time
 
 class ParamsType(IntEnum):
     """Sound direction enum
     """
-    SENDER_UID = 0
-    MESSAGE_UID = 1
+    UID = 0
 
 
 class MessageBeaconParser(IParser):
@@ -24,32 +22,21 @@ class MessageBeaconParser(IParser):
         The message returned should ressemble something like that:
             "trentorianUid"
         """
-        return f'{trentorian.uid}_{str(time())}'
+        return f"{trentorian.uid}"
 
     def deserialize(self, trentorian: Trantorian, message_content: str, message_hitpoint: int) -> Trantorian:
         """Deserialize the message content, for birth info
         """
-        params = message_content.split("_")
-
-        if len(params)!= 2:
-            warn(f"Invalid message content for beacon message: {message_content}")
-            return trentorian
         if trentorian.state == "beacon":
             try:
-                if float(trentorian.uid) < float(params[ParamsType.SENDER_UID]):
+                if float(trentorian.uid) < float(message_content):
                     return trentorian
             except ValueError:
                 warn(f"Invalid message content for beacon message: {message_content}")
                 return trentorian
 
-        if params[ParamsType.MESSAGE_UID] == trentorian.last_beacon_message_uid:
-            trentorian.state = "wander"
-            return trentorian
-
-        trentorian.last_beacon_message_uid = params[ParamsType.MESSAGE_UID]
-
         trentorian.state = "going somewhere"
-        trentorian.last_beacon_uid = params[ParamsType.SENDER_UID]
+        trentorian.last_beacon_uid = message_content
         trentorian.last_beacon_direction = message_hitpoint
 
         return trentorian

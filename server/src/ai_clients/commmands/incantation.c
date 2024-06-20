2baa6b9d557518ecea272196bbd0a766a7b1470f
Clément <clement2104.boillot@gmail.com>
2024-06-20 20:19:52 +0200

    feat(server): merge incnatation INC_NEEDS

--- server/src/ai_clients/commmands/incantation.c
+++ server/src/ai_clients/commmands/incantation.c
@@ -5,8 +5,6 @@
 ** incantation
 */
 
-#include "incantation.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -15,6 +13,37 @@
 #include "commands.h"
 #include "gui_protocols/commands/commands.h"
 #include "server.h"
+
+static const payload_t INC_NEEDS[7] = {
+    {
+        {{FOOD, 0}, {LINEMATE, 1}, {DERAUMERE, 0}, {SIBUR, 0},
+            {MENDIANE, 0}, {PHIRAS, 0}, {THYSTAME, 0}, {EGG, 0}, {PLAYER, 1}}
+    },
+    {
+        {{FOOD, 0}, {LINEMATE, 1}, {DERAUMERE, 1}, {SIBUR, 1},
+            {MENDIANE, 0}, {PHIRAS, 2}, {THYSTAME, 0}, {EGG, 0}, {PLAYER, 2}}
+    },
+    {
+        {{FOOD, 0}, {LINEMATE, 2}, {DERAUMERE, 0}, {SIBUR, 1},
+            {MENDIANE, 0}, {PHIRAS, 2}, {THYSTAME, 0}, {EGG, 0}, {PLAYER, 2}}
+    },
+    {
+        {{FOOD, 0}, {LINEMATE, 1}, {DERAUMERE, 1}, {SIBUR, 2},
+            {MENDIANE, 0}, {PHIRAS, 1}, {THYSTAME, 0}, {EGG, 0}, {PLAYER, 4}}
+    },
+    {
+        {{FOOD, 0}, {LINEMATE, 1}, {DERAUMERE, 2}, {SIBUR, 2},
+            {MENDIANE, 3}, {PHIRAS, 0}, {THYSTAME, 0}, {EGG, 0}, {PLAYER, 4}}
+    },
+    {
+        {{FOOD, 0}, {LINEMATE, 1}, {DERAUMERE, 2}, {SIBUR, 3},
+            {MENDIANE, 0}, {PHIRAS, 1}, {THYSTAME, 0}, {EGG, 0}, {PLAYER, 6}}
+    },
+    {
+        {{FOOD, 0}, {LINEMATE, 2}, {DERAUMERE, 2}, {SIBUR, 2},
+            {MENDIANE, 2}, {PHIRAS, 1}, {THYSTAME, 0}, {EGG, 0}, {PLAYER, 6}}
+    },
+};
 
 static bool can_incantation(ai_client_t *client, const cell_t *cell)
 {

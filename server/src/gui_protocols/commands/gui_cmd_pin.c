b408f5bd5d82e4a8b9d32cfbec1f44e1426e2faf
Zetsumeii <theophile.combes@epitech.eu>
2024-06-21 17:52:19 +0200

    fix: forget the command

--- server/src/gui_protocols/commands/gui_cmd_pin.c
+++ server/src/gui_protocols/commands/gui_cmd_pin.c
@@ -24,7 +24,7 @@ void gui_cmd_pin(server_t *server, gui_client_t *client, char *args)
         return;
     }
     gui_dprintf(
-        client, "%d %d %d %d %d %d %d %d %d %d\n", client_id, current->pos.x,
+        client, "pin %d %d %d %d %d %d %d %d %d %d\n", client_id, current->pos.x,
         current->pos.y, current->res[FOOD].quantity,
         current->res[LINEMATE].quantity, current->res[DERAUMERE].quantity,
         current->res[SIBUR].quantity, current->res[MENDIANE].quantity,

4272ce26b712446e8c6384303136e2742bb58256
Zetsumeii <theophile.combes@epitech.eu>
2024-06-21 19:55:35 +0200

    fix: plv kinda cringe

--- server/src/gui_protocols/commands/gui_cmd_plv.c
+++ server/src/gui_protocols/commands/gui_cmd_plv.c
@@ -14,14 +14,11 @@
 void gui_cmd_plv(server_t *server, gui_client_t *client, char *args)
 {
     char **cmd = my_str_to_word_array(args, " ");
-    int id = 0;
+    int id = (cmd && *cmd) ? atoi(*cmd) : -1;
     ai_client_t *current = NULL;
 
-    if (client == NULL || current == NULL)
-        return;
-    id = atoi(*cmd);
     current = get_client_by_id(server, id);
-    if (current == NULL)
+    if (client == NULL || current == NULL)
         return;
     gui_dprintf(client, "plv %d %d\n", id, current->lvl);
 }

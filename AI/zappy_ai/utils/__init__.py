208191909226223743f5075ff9148c43647159b5
Clément <clement2104.boillot@gmail.com>
2024-06-23 18:35:35 +0200

    hotfix: fix pyproject

---
+++ AI/zappy_ai/utils/__init__.py
@@ -0,0 +1,7 @@
+from .utils import (
+    LEVELS, get_all_subsets, get_incantation_team, pack_infos,
+    unpack_infos, split_list, check_levelup, determine_direction
+)
+
+__all__ = ["LEVELS", "get_all_subsets", "get_incantation_team", "pack_infos",
+           "unpack_infos", "split_list", "check_levelup", "determine_direction"]

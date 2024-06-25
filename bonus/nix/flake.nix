0547033b8a8d47e66104d05c8aff82cd662d1072
Clément <clement2104.boillot@gmail.com>
2024-06-25 18:45:09 +0200

    build: update nix flake
    - add docker image
    - add zappy_ai & zappy_gui
    - fix nix shell for gui

--- bonus/nix/flake.nix
+++ bonus/nix/flake.nix
@@ -8,25 +8,74 @@
     inputs.utils.lib.eachDefaultSystem (
       system: let
         pkgs = import inputs.nixpkgs {inherit system;};
+        python = pkgs.python312;
       in {
         formatter = pkgs.alejandra;
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.packages.${system} ++ (with pkgs; [glfw assimp]);
-          packages = with pkgs; [clang-tools_18 strace aflplusplus python312];
+          inputsFrom = builtins.attrValues self.packages.${system};
+          packages = with pkgs; [clang-tools_18 strace aflplusplus];
           env.MAKEFLAGS = "-j12";
+          env.LD_LIBRARY_PATH = "${pkgs.libGL}/lib";
         };
 
-        packages = {
+        packages = rec {
           zappy_server = pkgs.stdenv.mkDerivation {
             name = "zappy_server";
             src = ../../server;
-            nativeBuildInputs = with pkgs; [cmake libuuid];
+            nativeBuildInputs = with pkgs; [cmake];
             enableParallelBuilding = true;
           };
+          zappy_gui = pkgs.stdenv.mkDerivation {
+            name = "zappy_gui";
+            src = ../../GUI;
+            nativeBuildInputs = with pkgs; [cmake makeWrapper pkg-config];
+            buildInputs = with pkgs; [
+              wayland
+              wayland-protocols
+              libxkbcommon
+
+              assimp
+              glfw
+              libGL
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXinerama
+              xorg.libXrandr
+              zlib
+            ];
+            propagatedBuildInputs = with pkgs; [ libGL ];
+
+            postInstall = ''
+              wrapProgram $out/bin/zappy_gui \
+                --prefix LD_LIBRARY_PATH : "${pkgs.libGL}/lib"
+            '';
+            enableParallelBuilding = true;
+          };
+          zappy_ai = python.pkgs.buildPythonPackage {
+            pname = "zappy_ai";
+            version = "0.1.0";
+            src = ../../AI;
+            pyproject = true;
+            build-system = with python.pkgs; [setuptools];
+            dependencies = with python.pkgs; [typer];
+          };
+
+          zappy_server_docker = pkgs.dockerTools.buildImage {
+            name = "zappy_server";
+            tag = "latest";
+            created = "now";
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = [zappy_server];
+            };
+            config.Cmd = ["zappy_server" "-p" "52001" "-x" "10" "-y" "10" "-n" "team1" "-c" "12" "-f" "100"];
+          };
+
           reference_gui = pkgs.buildFHSUserEnv {
             name = "reference_gui-fhs-shell";
-            targetPkgs = p: [p.sfml] ;
+            targetPkgs = p: [p.sfml];
             runScript = "env --chdir=${../ref} -S ./zappy_gui";
           };
         };
